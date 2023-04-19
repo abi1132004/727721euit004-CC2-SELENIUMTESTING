7402ee020fbc36adc11b0a5b91b450e2bf025da6
abi1132004 <118493136+abi1132004@users.noreply.github.com>
2023-04-19 12:57:13 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,42 @@
+package com.selenium.test2.selenium1;
+
+import static org.testng.Assert.assertTrue;
+
+import org.apache.xmlbeans.impl.tool.XSTCTester.TestCase;
+
+import com.aventstack.extentreports.model.Test;
+
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
